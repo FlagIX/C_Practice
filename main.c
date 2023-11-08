5e6040129fff31e68f5c2d1618e2272ff49c032a
blessingIX <1350437095@qq.com>
2023-11-08 22:05:35 +0800

    3.3

--- main.c
+++ main.c
@@ -20,7 +20,18 @@ void question3_2()
 
 void question3_3()
 {
-
+    double deposit;
+    double rate = 0.0225;
+    double capital = 0;
+    int n = 0;
+    scanf_s("%lf%d", &capital, &n);
+    double tmp = 1;
+    for (int i = 0; i < n; ++i) {
+        rate += 1;
+        tmp *= rate;
+    }
+    deposit = capital * tmp;
+    printf("%lf", deposit);
 }
 
 int main()
